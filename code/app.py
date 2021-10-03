7eb19cc8642872b07fad26386ec49e5690330bcf
Ishan Bhatt <ivbhatt@outlook.com>
2021-10-03 19:05:38 -0400

    importing all datasets

--- code/app.py
+++ code/app.py
@@ -7,38 +7,106 @@ import pandas as pd
 
 import dash
 from dash import html, dcc
+from pandas.core.frame import DataFrame
 import plotly.express as px
+
+from utils import print_info, print_warn
+
 
 app = dash.Dash(__name__)
 
-S1_PATH = os.path.join("..","data","S1","data_cleaned", "data.csv")
-S2_PATH = os.path.join("..","data","S2","data_cleaned", "data.csv")
+
+DATA_PATH = os.path.join("..", "data")
+
+datasets = os.listdir(DATA_PATH)
+
+if len(datasets) != 8:
+    print_warn("All 8 datasets not found." )
+
+print_info("Datasets:", datasets)
+
+
+dataFrame = pd.DataFrame(columns=["Dataset", "Location", "Year", "Month", "Day", "Weekday", "Hour","Demand"])
+
+for dataset in datasets:
+    for root, dirs, files in os.walk(os.path.join(DATA_PATH, dataset, "data_cleaned")):
+        print_info("Working on dataset:", dataset)
+        if "Date.csv" in files:
+            date = pd.read_csv(os.path.join(root, "Date.csv"))
+
+            month_series = date["Month"]
+
+            curr_year = 1
+            year_series = [0 for i in month_series]
+
+            for i in range(len(month_series)-1):
+                if month_series[i+1] >= month_series[i]:
+                    year_series[i] += curr_year
+                else:
+                    year_series[i] += curr_year
+                    curr_year+=1
+
+            year_series[-1] += curr_year
+
+            for file in files:
+                if file != "Date.csv":
+                    print_info("Working on dataset:", dataset," location:", file.split(".")[0])
+
+                    t = pd.read_csv(os.path.join(root, file))
+                    
+                    if "Demand" in t.columns:
+                        temp_df = pd.DataFrame(data = t["Demand"])
+                    else:
+                        temp_df = pd.DataFrame(data = t["Net"])
+                        temp_df["Demand"] = temp_df["Net"]
+                        temp_df.drop(columns=["Net"])
+
+
+                    temp_df["Dataset"] = dataset
+                    temp_df["Location"] = file.split(".")[0]
+                    temp_df["Year"] = year_series
+                    temp_df["Month"] = month_series
+                    temp_df["Day"] = date["Day"]
+                    temp_df["Hour"] = date["Hour"]
+                    temp_df["Weekday"] = date["Weekday"]
+                        
+
+                    dataFrame = pd.concat([dataFrame, temp_df])
+
+        else:
+            temp_df = pd.read_csv(os.path.join(root, files[0]))[["Month","Day","Weekday","Hour","Demand"]]
+            temp_df["Dataset"] = dataset
+            temp_df["Location"] = "-"
+            temp_df["Year"] = "1"
+
+            dataFrame = pd.concat([dataFrame, temp_df])
+
+# print(dataFrame.groupby(["Dataset", "Location", "Year"]).size())
 
 
 
-s1 = pd.read_csv(S1_PATH)
-s1["dataset"] = "s1"
+hour_demand = px.box(dataFrame[["Hour", "Demand", "Weekday"]], y = "Demand", x = "Hour", color = "Weekday")
+month_demand = px.box(dataFrame[["Month", "Demand",  "Weekday"]], y = "Demand", x = "Month", color = "Weekday")
+year_demand = px.box(dataFrame[["Year", "Demand",  "Weekday"]], y = "Demand", x = "Year", color = "Weekday")
 
-s2 = pd.read_csv(S2_PATH)
-s2["dataset"] = "s2"
-
-s = pd.concat([s1, s2])
-
-hour_demand = px.box(s[["Hour", "Demand", "Weekday"]], y = "Demand", x = "Hour", color = "Weekday")
-month_demand = px.box(s[["Month", "Demand",  "Weekday"]], y = "Demand", x = "Month", color = "Weekday")
-
-
-heatmap = px.imshow(np.array(s.groupby(["Month", "Hour"])["Demand"].mean()).reshape(12, 24), color_continuous_scale="icefire")
+heatmap = px.imshow(np.array(dataFrame.groupby(["Month", "Hour"])["Demand"].mean()).reshape(12, 24), color_continuous_scale="icefire")
 
 app.layout = html.Div(children=[
     html.H1(id = "heading", children="Solar power dashboard"),
     dcc.RadioItems(id = "data_selector", options = [
-        {"label":"s1", "value":"s1"},
-        {"label":"s2", "value":"s2"},
-    ], value = "s1"
+        {"label":"S1", "value":"S1"},
+        {"label":"S2", "value":"S2"},
+        {"label":"S3", "value":"S3"},
+        {"label":"S4", "value":"S4"},
+        {"label":"L1", "value":"L1"},
+        {"label":"L2", "value":"L2"},
+        {"label":"L3", "value":"L3"},
+        {"label":"L4", "value":"L4"},
+    ], value = "S1"
     ),
     dcc.Graph(id = "hour_demand", figure=hour_demand),
     dcc.Graph(id = "month_demand", figure=month_demand),
+    dcc.Graph(id = "year_demand", figure=year_demand),
     dcc.Graph(id = "heatmap",figure = heatmap)
 ])
 
@@ -46,9 +114,9 @@ app.layout = html.Div(children=[
     Output(component_id="hour_demand", component_property="figure"),
     Input(component_id="data_selector", component_property="value"))
 def update_dataset(selected_dataset):
-    filtered_s = s[s.dataset == selected_dataset]
+    filtered_dataFrame = dataFrame[dataFrame.Dataset == selected_dataset]
 
-    hour_demand = px.box(filtered_s[["Hour", "Demand", "Weekday"]], y = "Demand", x = "Hour", color = "Weekday")
+    hour_demand = px.box(filtered_dataFrame[["Hour", "Demand", "Weekday"]], y = "Demand", x = "Hour", color = "Weekday")
 
     hour_demand.update_layout(transition_duration = 500)
 
@@ -59,22 +127,33 @@ def update_dataset(selected_dataset):
     Output(component_id="month_demand", component_property="figure"),
     Input(component_id="data_selector", component_property="value"))
 def update_dataset(selected_dataset):
-    filtered_s = s[s.dataset == selected_dataset]
+    filtered_dataFrame = dataFrame[dataFrame.Dataset == selected_dataset]
 
-    month_demand = px.box(filtered_s[["Month", "Demand", "Weekday"]], y = "Demand", x = "Month", color = "Weekday")
+    month_demand = px.box(filtered_dataFrame[["Month", "Demand", "Weekday"]], y = "Demand", x = "Month", color = "Weekday")
 
     month_demand.update_layout(transition_duration = 500)
 
     return month_demand
 
+@app.callback(
+    Output(component_id="year_demand", component_property="figure"),
+    Input(component_id="data_selector", component_property="value"))
+def update_dataset(selected_dataset):
+    filtered_dataFrame = dataFrame[dataFrame.Dataset == selected_dataset]
+
+    year_demand = px.box(filtered_dataFrame[["Year", "Demand", "Weekday"]], y = "Demand", x = "Year", color = "Weekday")
+
+    year_demand.update_layout(transition_duration = 500)
+
+    return year_demand
 
 @app.callback(
     Output(component_id="heatmap", component_property="figure"),
     Input(component_id="data_selector", component_property="value"))
 def update_dataset(selected_dataset):
-    filtered_s = s[s.dataset == selected_dataset]
+    filtered_dataFrame = dataFrame[dataFrame.Dataset == selected_dataset]
 
-    heatmap = px.imshow(np.array(filtered_s.groupby(["Month", "Hour"])["Demand"].mean()).reshape(12, 24), color_continuous_scale="icefire")
+    heatmap = px.imshow(np.array(filtered_dataFrame.groupby(["Month", "Hour"])["Demand"].mean()).reshape(12, 24), color_continuous_scale="icefire")
 
 
     heatmap.update_layout(transition_duration = 500)
---
+++ code/utils.py
@@ -0,0 +1,39 @@
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def info(func):
+    def inner(*args, **kwargs):
+        print(bcolors.OKCYAN + "[INFO]", end = "\t")
+        func(*args, **kwargs)
+        print(bcolors.ENDC, end = "")
+    return inner
+
+def warn(func):
+    def inner(*args, **kwargs):
+        print(bcolors.WARNING + "[WARN]", end = "\t")
+        func(*args, **kwargs)
+        print(bcolors.ENDC, end = "")
+
+    return inner
+
+
+
+
+
+@info
+def print_info(*args, **kwargs):
+    print(*args, **kwargs)
+
+@warn
+def print_warn(*args, **kwargs):
+    print(*args, **kwargs)
